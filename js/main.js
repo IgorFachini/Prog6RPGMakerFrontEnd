9bf9a63b782f9824a7756dfcf334f7a3e80de124
igorfachini <igor.fachini@catolicasc.org.br>
2017-11-27 20:33:13 -0200

    currentPage with class active indicate

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 var navbarVm = "";
 // registro
 let navbarComponent = {
-  template : `	<nav id="primary-navigation">
+  template: `	<nav id="primary-navigation">
   <ul class="nav-list">
     <li>
-      <a href="index.html" class="active">HOME</a>
+      <a :class="{active: isCurrentPage('index.html')}" href="index.html">HOME</a>
     </li>
     <li class="has-children">
       <a href="#" class="">Rpg</a>
       <ul>
         <li>
-          <a href="./world_of_darkness.html"  class="">Criar personagem</a>
+          <a :class="{active: isCurrentPage('world_of_darkness.html')}" href="./world_of_darkness.html"  class="">Criar personagem</a>
         </li>
         <li>
           <p class="coming-soon">
@@ -24,48 +24,51 @@ let navbarComponent = {
       <a href="#" class="">TestCrud</a>
       <ul>
         <li>
-          <a href="./users.html">UserList</a>
+          <a :class="{active: isCurrentPage('users.html')}" href="./users.html">UserList</a>
         </li>
         <li>
-          <a href="./userform.html">UserCreat</a>
+          <a :class="{active: isCurrentPage('userform.html')}" href="./userform.html">UserCreat</a>
         </li>
         <li>
-          <a href="./players.html">PlayerList</a>
+          <a :class="{active: isCurrentPage('players.html')}" href="./players.html">PlayerList</a>
         </li>
         <li>
-          <a href="./playerform.html">PlayerCreat</a>
+          <a :class="{active: isCurrentPage('playerform.html')}" href="./playerform.html">PlayerCreat</a>
         </li>
         <li>
-          <a href="./chronicles.html">ChronicleList</a>
+          <a :class="{active: isCurrentPage('chronicles.html')}" href="./chronicles.html">ChronicleList</a>
         </li>
         <li>
-          <a href="./chronicleform.html">ChronicleCreat</a>
+          <a :class="{active: isCurrentPage('chronicleform.html')}" href="./chronicleform.html">ChronicleCreat</a>
         </li>
       </ul>
     </li>
     <li v-if="!this.userData">
-      <a href="#" class="">Login</a>
-      <a href="#" class="">Cadastra</a>					
+      <a :class="{active: isCurrentPage('signin.html')}" href="#" class="">Login</a>
+      <a :class="{active: isCurrentPage('signup.html')}" href="#" class="">Cadastra</a>					
     </li>
     <li v-if="this.userData">
       <a href="#" class="">Ola usuario</a>											
     </li>
   </ul>
   <div class="close"></div>
-</nav>`
+</nav>`,
+  methods: {
+    isCurrentPage: function(page) {
+      console.log(page);
+      return window.location.pathname.split("/").pop() == page;
+    }
+  }
 };
 navbarVm = new Vue({
   el: "#navBarContainer",
   data: {
-    userData: "",
-    
+    userData: ""
   },
   methods: {
-    isCurrentPage: function(page){
-      return window.location.pathname.split("/").pop() == page;      
-    }
+   
   },
   components: {
-    'navbar-component': navbarComponent
+    "navbar-component": navbarComponent
   }
 });
