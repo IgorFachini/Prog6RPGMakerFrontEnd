026a2a24a484d796c25a05d44125b17cbb916298
igorfachini <igor.fachini@catolicasc.org.br>
2017-12-07 00:06:49 -0200

    ïntegration crud player, user

--- js/data.js
+++ js/data.js
@@ -1,21 +1,54 @@
-function sendOperation(type,data) {
+function sendOperation(operation, type, data) {
+  let url = `http://localhost:33492/${type}`;
+  switch (operation) {
+    case "GET":
+    case "PUT":
+    case "DELETE":
+      data ? url += `/${data.Id}` : "";
+      break;
+  }
+  console.log("URL",url);  
+  console.log(operation,JSON.stringify(data));
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: ``,
-      type: type,
+      url: url,
+      type: operation,
+      headers: { "Content-Type": "application/json" },
       data: JSON.stringify(data),
       success: function(data, textStatus, jqXHR) {
         console.log("success", data);
+        console.log(jqXHR, textStatus);
+        resolve(data);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log("error", errorThrown);
+        console.log(jqXHR, textStatus);
+        reject(errorThrown);
       }
     });
   });
 }
 
-var UserModel = {id: "", name: "", email: "", password: ""}
+var UserModel = {
+  Id: "0",
+  Email: "",
+  NickName: ""
+};
 
+var PlayerModel = {
+  Name: "",
+  Chronicles: [],
+  GameCharacters: [],
+  Id: "0"
+};
 
+var ChronicleModel = {
+  Name: "",
+  Players: [],
+  GameType: 0,
+  GameMaster: {},
+  Id: 0
+}
 
-
+var GameTypes = [];
+GameTypes.push({Name: "World Of Darknes", Value: 0})
